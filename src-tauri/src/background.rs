2cf1710a2f568a9248ca656c7de86a4dabc473ae
Del Wang <wjg12341234@163.com>
2025-12-20 21:05:40 +0800

    fix: 修复 background 窗口样式

--- src-tauri/src/background.rs
+++ src-tauri/src/background.rs
@@ -1,16 +1,25 @@
 // 创建全局隐藏窗口，用来做一些后台任务，如初始化应用等
-pub fn create_background_window(app: &tauri::AppHandle) {
-    let _ = tauri::WebviewWindowBuilder::new(
+pub fn create_background_window(app: &tauri::AppHandle) -> Result<(), String> {
+    let win = tauri::WebviewWindowBuilder::new(
         app,
         "background",
         tauri::WebviewUrl::App("index.html#/background".into()),
     )
     .title("background")
     .inner_size(0.0, 0.0)
+    .position(0.0, 0.0)
     .decorations(false)
     .transparent(true)
     .skip_taskbar(true)
+    .shadow(false)
+    .resizable(false)
     .always_on_bottom(true)
     .build()
-    .map_err(|e| e.to_string());
+    .map_err(|e| e.to_string())?;
+
+    // 在 Windows 上窗口大小做不到 0，所以隐藏掉鼠标事件
+    win.set_ignore_cursor_events(true)
+        .map_err(|e| e.to_string())?;
+
+    Ok(())
 }
